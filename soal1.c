ad146ed2e453b11157d1e7be653872fb44c31323
hanif-page <amrhanif15@gmail.com>
2026-04-13 09:00:54 +0700

    feat: kode kembali ke versi awal, yaitu deklarasi variabel tanpa pointer

--- soal1.c
+++ soal1.c
@@ -6,36 +6,34 @@
  *   Deskripsi           : Kode ini merupakan solusi dari Praktikum EL2008, Modul 1, Soal 1, yaitu Gerbang Koloni Bertingkat. Permasalahan berupa sebuah gerbang keamanan untuk memproses data satu peneliti. Tujuan utama dari program ini adalah menentukan status akhir peneliti berdasarkan level izin, suhu tubuh, kadar radiasi, dan jam kedatangan.
  */
 
-//  Catatan: Kode ini merupakan versi ke-2 dari kode, yaitu dengan menggunakan pointer pada deklarasi variabel.
+#include <stdio.h>
+#include <string.h>
 
- #include <stdio.h>
- #include <string.h>
-  
- int main(void) {
-    int *izin, *suhu, *radiasi, *jam; // memberikan deklarasi pointer variabel yang akan digunakan.
+int main(void) {
+    int izin, suhu, radiasi, jam; // memberikan deklarasi variabel yang akan digunakan
 
-    scanf("%d %d %d %d", izin, suhu, radiasi, jam); // menerima input dari user dengan pass variabel masukkan input ke pointer variabel.
+    scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam); // menerima input dari user
 
     // validasi kondisi berdasarkan permintaan problem
-    if(*radiasi >= 6){
+    if(radiasi >= 6){
         printf("TOLAK");
     }
-    else if (*suhu >= 390){
+    else if (suhu >= 390){
         printf("KARANTINA");
     } 
-    else if (*izin == 1 && (*jam < 6 || *jam > 20)){
+    else if (izin == 1 && (jam < 6 || jam > 20)){
         printf("TOLAK");
     }
-    else if (*izin == 1){
+    else if (izin == 1){
         printf("MASUK");
     }
-    else if (*izin == 2 && (*radiasi <= 2 && (*jam >= 8 && *jam <= 18))){
+    else if (izin == 2 && (radiasi <= 2 && (jam >= 8 && jam <= 18))){
         printf("MASUK");
     }
-    else if (*izin == 2){
+    else if (izin == 2){
         printf("PEMERIKSAAN");
     }
-    else if (*izin == 3 && *radiasi == 0 && *suhu < 380){
+    else if (izin == 3 && radiasi == 0 && suhu < 380){
         printf("MASUK");
     }
     else {
